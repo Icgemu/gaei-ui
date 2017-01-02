0d72b234b169e44952ac66267fb70ab7af8ce9ae
eshgfuu <437686483@qq.com>
2017-01-02 20:35:36 +0800

    edit

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -17,6 +17,7 @@ const config = {
       }
     },
     entry: [
+      'babel-polyfill',
       'webpack-dev-server/client?http://0.0.0.0:3000',
       'webpack/hot/dev-server',
       path.join(__dirname, 'src/components/index/index')
